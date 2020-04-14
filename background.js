2d94bed641e7fa44025b6b3f4f6376391a2dc4a8
maxdevjs <max.devjs@gmail.com>
2020-04-14 04:36:39 -0300

    Improve extension
    - badge
    - local storage
    - enable / disable checks

--- background.js
+++ background.js
@@ -1,10 +1,25 @@
 const link = 'https://www.youtube.com/playlist?list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO';
+let enabled;
+
+const firstRun = () => {
+  browser.storage.local.get(('enabled'), result => {
+    enabled = result.enabled;
+    // First run after installation: set enabled to true
+    if (typeof(enabled) === "undefined") {
+      chrome.storage.local.set({'enabled': true});
+    }
+    setBadge(enabled);
+  })
+}
+firstRun();
 
 const checkTime = () => {
   let date = new Date();
   let hours = date.getHours();
+
   // Will check between 8pm and 11pm.
-  if ((hours > 20) && (hours < 23)) {
+  if ((hours < 23) && enabled) {
+  // if ((hours > 20) && (hours < 23) && enabled) {
     // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/Tabs/create
     const creating = browser.tabs.create({
       url: link
@@ -12,18 +27,6 @@ const checkTime = () => {
     creating.then(onCreated, onError);
   }
 }
-
-// On start up, check the time to see what theme to show.
-// checkTime();
-
-// Set up an alarm to check this regularly.
-// delayInMinutes: The time the alarm will fire first, given as minutes from the time the alarm is set
-// periodInMinutes: If this is specified, the alarm will fire again every periodInMinutes after its initial firing
-// Will check 1 minute after the extension is loaded, and then at intervals of 120 minutes.
-browser.alarms.onAlarm.addListener(checkTime);
-
-// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/alarms/create
-browser.alarms.create('checkTime', {delayInMinutes: 1, periodInMinutes: 120});
 
 const onCreated = (tab) => {
   console.log(`Created new tab: ${tab.id}`)
@@ -33,3 +36,40 @@ const onError = (error) => {
   console.log(`Error: ${error}`);
 }
 
+const setBadge = enabled => {
+  let status = enabled ? 'Disable' : 'Enable';
+  let title = `${status} Svelte Playlist`;
+
+  chrome.browserAction.setBadgeText({text: enabled ? '' : 'Off'});
+  chrome.browserAction.setTitle({title: title});
+
+  chrome.storage.local.set({'enabled': enabled});
+  // browser.storage.local.get('enabled').then(console.log("enabled or not", enabled), onError); 
+}
+
+// Enable / disable the extension's checks
+const toggleEnabled = () => {
+  setBadge(!enabled);
+  enabled = !enabled;
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggleEnabled(!enabled);
+});
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   browser.storage.local.get(('enabled'), function(result) {
+//       setEnabled(!result.enabled);
+//   });
+// });
+
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/alarms/create
+//
+// Set up an alarm for regular checks.
+// delayInMinutes: The time the alarm will fire first, given as minutes from the time the alarm is set
+// periodInMinutes: If this is specified, the alarm will fire again every periodInMinutes after its initial firing
+// Will check 1 minute after the extension is loaded, and then at intervals of 120 minutes.
+// browser.alarms.create('checkTime', {delayInMinutes: 1, periodInMinutes: 120});
+// Test purpose: 0.1 minutes after installation and every 0.3 minutes
+browser.alarms.create('checkTime', {delayInMinutes: 0.1, periodInMinutes: 0.3});
+
+browser.alarms.onAlarm.addListener(checkTime);
